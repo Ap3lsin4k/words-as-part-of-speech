31569a3511e13050bd90da4d8e41de9a16e02d97
Andrii Fedorko <andrii.fedorko01@gmail.com>
2020-12-11 14:42:42 +0200

    Removed .idea files from repository
    git rm -r --cached .
    rm is the remove command
    
    -r will allow recursive removal
    –cached will only remove files from the index. Your files will still be there.
    The . indicates that all files will be untracked. You can untrack a specific file with git rm --cached foo.txt

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='PartsOfSpeech',
-    extras_reqiure=dict(tests=['pytest']),
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-)
